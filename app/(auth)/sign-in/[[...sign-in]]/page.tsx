fd77448dec29d3d327600ff31e642f07bc6c87e8
natanaelnatanael <branimirvrnoga@gmail.com>
2025-08-18 14:02:53 +0200

    Update page.tsx

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,3 +1,4 @@
 "use client";
 import { SignIn } from "@clerk/nextjs";
 export default function Page(){ return <main style={{display:"grid",placeItems:"center",minHeight:"100vh"}}><SignIn routing="path" path="/sign-in" /></main>; }
+
