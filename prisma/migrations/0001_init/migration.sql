f3d1fe54ceb72129598824e1cb2fdd8476954d8e
natanaelnatanael <branimirvrnoga@gmail.com>
2025-08-19 13:35:51 +0200

    Update migration.sql

--- prisma/migrations/0001_init/migration.sql
+++ prisma/migrations/0001_init/migration.sql
@@ -1,41 +1,26 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" TEXT NOT NULL,
-    "clerkId" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "plan" TEXT NOT NULL DEFAULT 'free',
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+  "id" TEXT PRIMARY KEY,
+  "clerkId" TEXT NOT NULL UNIQUE,
+  "email" TEXT NOT NULL UNIQUE,
+  "plan" TEXT NOT NULL DEFAULT 'free',
+  "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
--- CreateTable
 CREATE TABLE "Job" (
-    "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "Job_pkey" PRIMARY KEY ("id")
+  "id" TEXT PRIMARY KEY,
+  "userId" TEXT NOT NULL,
+  "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
--- CreateTable
 CREATE TABLE "StripeCustomer" (
-    "id" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "stripeId" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "StripeCustomer_pkey" PRIMARY KEY ("id")
+  "id" TEXT PRIMARY KEY,
+  "userId" TEXT NOT NULL UNIQUE,
+  "stripeId" TEXT NOT NULL UNIQUE,
+  "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
--- CreateIndex
-CREATE UNIQUE INDEX "User_clerkId_key" ON "User"("clerkId");
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
-CREATE UNIQUE INDEX "StripeCustomer_userId_key" ON "StripeCustomer"("userId");
-CREATE UNIQUE INDEX "StripeCustomer_stripeId_key" ON "StripeCustomer"("stripeId");
-
--- AddForeignKey
-ALTER TABLE "Job" ADD CONSTRAINT "Job_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
-ALTER TABLE "StripeCustomer" ADD CONSTRAINT "StripeCustomer_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "Job"
+  ADD CONSTRAINT "Job_userId_fkey"
+  FOREIGN KEY ("userId") REFERENCES "User"("id")
+  ON DELETE RESTRICT ON UPDATE CASCADE;
