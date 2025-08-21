46285d527c4a4a432eaad00a70f994c58f20b3ac
natanaelnatanael <branimirvrnoga@gmail.com>
2025-08-21 15:04:34 +0200

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,9 +1,5 @@
 import Stripe from "stripe";
 
-if (!process.env.STRIPE_SECRET_KEY) {
-  throw new Error("Missing STRIPE_SECRET_KEY");
-}
-
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
   apiVersion: "2024-06-20",
 });
