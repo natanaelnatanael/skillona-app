844cdcd857e12ee55716f7367a8b90ec4947618a
natanaelnatanael <branimirvrnoga@gmail.com>
2025-08-21 13:44:10 +0200

    Update limits.ts

--- LIB/limits.ts
+++ LIB/limits.ts
@@ -1,1 +1,15 @@
+export const FREE_PLAN_LIMIT = 3; // broj besplatnih videa
+export const BASIC_PLAN_LIMIT = 30;
+export const PRO_PLAN_LIMIT = 999;
+
+export function getLimitByPlan(plan: string) {
+  switch (plan) {
+    case "basic":
+      return BASIC_PLAN_LIMIT;
+    case "pro":
+      return PRO_PLAN_LIMIT;
+    default:
+      return FREE_PLAN_LIMIT;
+  }
+}
 
