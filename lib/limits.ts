5ba6e1a78d2d7a183d447179b46ea7a96656729c
natanaelnatanael <branimirvrnoga@gmail.com>
2025-08-21 14:51:44 +0200

    Create limits.ts

---
+++ lib/limits.ts
@@ -0,0 +1,14 @@
+export const FREE_PLAN_LIMIT = 3;
+export const BASIC_PLAN_LIMIT = 30;
+export const PRO_PLAN_LIMIT = 999;
+
+export function getLimitByPlan(plan: string) {
+  switch (plan) {
+    case "basic":
+      return BASIC_PLAN_LIMIT;
+    case "pro":
+      return PRO_PLAN_LIMIT;
+    default:
+      return FREE_PLAN_LIMIT;
+  }
+}
