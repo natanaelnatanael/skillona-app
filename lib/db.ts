4526d3d0d2306b9e3adc5188c112a37f619d75be
natanaelnatanael <branimirvrnoga@gmail.com>
2025-08-23 21:15:20 +0100

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,18 +1,21 @@
 // lib/db.ts
 import { PrismaClient } from "@prisma/client";
 
-declare global {
-  // spriječi re-instanciranje u devu
-  // @ts-ignore
-  var prisma: PrismaClient | undefined;
+// držimo jednu instancu u devu (HMR)
+const globalForPrisma = globalThis as unknown as {
+  prisma: PrismaClient | undefined;
+};
+
+export const prisma: PrismaClient =
+  globalForPrisma.prisma ?? new PrismaClient();
+
+// alias – pa možeš importati i kao "db"
+export const db = prisma;
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
 }
 
-export const prisma =
-  // @ts-ignore
-  global.prisma || new PrismaClient();
-
-// @ts-ignore
-if (process.env.NODE_ENV !== "production") global.prisma = prisma;
 
 
 
