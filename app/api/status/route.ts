f21195486a9bee80591ceab258400f7ee0df06e9
natanaelnatanael <branimirvrnoga@gmail.com>
2025-08-23 21:16:54 +0100

    Update route.ts

--- app/api/status/route.ts
+++ app/api/status/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
-import { prisma } from "@/lib/db";
+import { db } from "@lib/db";
 export async function GET(req: NextRequest) {
   const jobId = req.nextUrl.searchParams.get("jobId") || "";
   const job = await prisma.job.findUnique({ where:{ id: jobId }});
