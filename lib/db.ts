e45d201c7bfa2f3352375c57ef3494343469dc71
natanaelnatanael <branimirvrnoga@gmail.com>
2025-08-24 20:08:11 +0100

    Update db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -1,15 +1,12 @@
-// lib/db.ts
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 
 export const db = globalForPrisma.prisma ?? new PrismaClient();
-
 // ✅ alias da radi i stari import: `import { prisma } from "@lib/db"`
 export const prisma = db;
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
-
 
 
 
