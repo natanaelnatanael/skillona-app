caa1e23856fb0e8ffc868a1fce056744bd555587
natanaelnatanael <branimirvrnoga@gmail.com>
2025-08-30 12:04:01 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { experimental: { typedRoutes: true } };
+const nextConfig = { reactStrictMode: true };
 export default nextConfig;
+
 
 
