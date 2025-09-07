ef9f28b5c662018f1dd3cba6cb0632be26c51328
natanaelnatanael <branimirvrnoga@gmail.com>
2025-09-07 19:08:34 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,10 @@
-// middleware.ts
 import { clerkMiddleware } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
 
-export default clerkMiddleware({
-  // Javno dostupne rute:
-  publicRoutes: ['/', '/sign-in(.*)', '/sign-up(.*)', '/api/status'],
-  // Zaštiti sve ispod /dashboard
-  ignoredRoutes: ['/api/status'],
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
-    // sve app-rute
-    '/((?!_next|.*\\..*).*)',
-    // uvijek middleware na ove:
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     '/(api|trpc)(.*)',
   ],
 };
