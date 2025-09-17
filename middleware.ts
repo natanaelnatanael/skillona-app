a83e7bda0a3a2e9751060b039e9fc2bc77111498
natanaelnatanael <branimirvrnoga@gmail.com>
2025-09-17 14:43:38 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,12 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware } from "@clerk/nextjs/server";
 
 export default clerkMiddleware();
 
 export const config = {
   matcher: [
+    // Skip Next internals & statics
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API
     '/(api|trpc)(.*)',
   ],
 };
-
